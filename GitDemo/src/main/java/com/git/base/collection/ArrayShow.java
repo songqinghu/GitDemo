d52181f47596a30adf4cf264bb99656f8be485f2
songqinghu <songqinghu@songqinghu.meixin.com>
2016-06-02 13:38:56 +0800

    数组初始化方式

---
+++ GitDemo/src/main/java/com/git/base/collection/ArrayShow.java
@@ -0,0 +1,12 @@
+package com.git.base.collection;
+
+public class ArrayShow {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+      int[] one= new int[3];
+      int[] two = new int[]{1,2,3,4,5};
+      int[] three = {1,2,3,4,5,5};
+    }
+
+}
