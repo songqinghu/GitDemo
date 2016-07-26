652c47bf5f361fe25835eefa49c0231c7de6bc31
songqinghu <sqh2010304012@126.com>
2016-07-26 23:00:18 +0800

    图片基本表格数据

---
+++ GitDemo/src/main/resources/sql/image.sql
@@ -0,0 +1,83 @@
+/*
+
+
+Date: 2016-07-26 22:27:05
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for ctoprepic
+-- ----------------------------
+DROP TABLE IF EXISTS `prepic`;
+CREATE TABLE `prepic` (
+  `pre_id` bigint(20) NOT NULL AUTO_INCREMENT comment '这是副表主键',
+  `web_id` bigint(20) NOT NULL  comment '抓取的web站点对应的Id,对应web站点信息表,区分抓取站点的信息',
+  `pictype` bigint(2) DEFAULT NULL comment '图片类型,依赖图片类型表',
+  `name` varchar(50) DEFAULT NULL comment '显示图片的名称',
+  `url` varchar(50) DEFAULT NULL   comment '下轮抓取的基础URL',
+  `murl` varchar(50) DEFAULT NULL   comment '小图片的URL,用于列表展示',
+  `cratedate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  comment '抓取时的创建时间',
+  `flag` int(11) DEFAULT '0'  comment '标识位,是否可用',
+  PRIMARY KEY (`pre_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1636 DEFAULT CHARSET=utf8;
+-- ----------------------------
+-- Table structure for ctomainpic
+-- ----------------------------
+DROP TABLE IF EXISTS `mainpic`;
+CREATE TABLE `mainpic` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT comment '详情图片,主表id',
+  `pre_id` bigint(20) DEFAULT NULL  comment '副表id',
+  `pictype` bigint(2) DEFAULT NULL comment '图片类型,依赖图片类型表',  
+  `name` varchar(50) DEFAULT NULL  comment '详情页展示名称',
+  `url` varchar(100) DEFAULT NULL  comment '详情页图片地址',
+  `cratedate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  comment '创建时间',
+  `flag` int(11) DEFAULT '0'  comment '图片标示位,默认可用',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15218 DEFAULT CHARSET=utf8;
+
+
+-- ----------------------------
+-- Table structure for ctomainpic
+-- ----------------------------
+DROP TABLE IF EXISTS `imagetype`;
+
+CREATE TABLE `imagetype` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT comment '类型Id',
+  `typename` varchar(50) DEFAULT NULL comment '图片列表名称',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16363 DEFAULT CHARSET=utf8;
+
+
+-- ----------------------------
+-- Table structure for ctomainpic
+-- ----------------------------
+DROP TABLE IF EXISTS `webinfo`;
+
+CREATE TABLE `webinfo` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT comment '抓取web站点标识id',
+  `location` varchar(50) DEFAULT NULL comment '抓取站点的url主地址',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16363 DEFAULT CHARSET=utf8;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
