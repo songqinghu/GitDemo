f447e588a5526db3fdd6ad5783adb70fc5e8d658
songqinghu <sqh2010304012@126.com>
2016-07-27 23:52:04 +0800

    抓取网站修改

--- GitDemo/src/main/resources/sql/image.sql
+++ GitDemo/src/main/resources/sql/image.sql
@@ -13,14 +13,14 @@ DROP TABLE IF EXISTS `prepic`;
 CREATE TABLE `prepic` (
   `pre_id` bigint(20) NOT NULL AUTO_INCREMENT comment '这是副表主键',
   `web_id` bigint(20) NOT NULL  comment '抓取的web站点对应的Id,对应web站点信息表,区分抓取站点的信息',
-  `pictype` bigint(2) DEFAULT NULL comment '图片类型,依赖图片类型表',
+  `pictype` bigint(3) DEFAULT NULL comment '图片类型,依赖图片类型表',
   `name` varchar(50) DEFAULT NULL comment '显示图片的名称',
   `url` varchar(50) DEFAULT NULL   comment '下轮抓取的基础URL',
   `murl` varchar(50) DEFAULT NULL   comment '小图片的URL,用于列表展示',
   `cratedate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  comment '抓取时的创建时间',
   `flag` int(11) DEFAULT '0'  comment '标识位,是否可用',
   PRIMARY KEY (`pre_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1636 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 -- ----------------------------
 -- Table structure for ctomainpic
 -- ----------------------------
@@ -28,13 +28,13 @@ DROP TABLE IF EXISTS `mainpic`;
 CREATE TABLE `mainpic` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT comment '详情图片,主表id',
   `pre_id` bigint(20) DEFAULT NULL  comment '副表id',
-  `pictype` bigint(2) DEFAULT NULL comment '图片类型,依赖图片类型表',  
+  `pictype` bigint(3) DEFAULT NULL comment '图片类型,依赖图片类型表',  
   `name` varchar(50) DEFAULT NULL  comment '详情页展示名称',
   `url` varchar(100) DEFAULT NULL  comment '详情页图片地址',
   `cratedate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  comment '创建时间',
   `flag` int(11) DEFAULT '0'  comment '图片标示位,默认可用',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15218 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 
 -- ----------------------------
@@ -46,7 +46,7 @@ CREATE TABLE `imagetype` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT comment '类型Id',
   `typename` varchar(50) DEFAULT NULL comment '图片列表名称',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16363 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 
 -- ----------------------------
@@ -58,12 +58,18 @@ CREATE TABLE `webinfo` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT comment '抓取web站点标识id',
   `location` varchar(50) DEFAULT NULL comment '抓取站点的url主地址',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16363 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 
+DROP TABLE IF EXISTS `webspiderinfo`;
 
-
-
+CREATE TABLE `webspiderinfo` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `web_id` varchar(50) DEFAULT NULL COMMENT '站点id',
+  `url` varchar(50) DEFAULT NULL COMMENT '抓取的url',
+  `pictype` bigint(3) DEFAULT NULL COMMENT '图片类型',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 
 
---
+++ GitDemo/src/main/resources/url/spider.sql
@@ -0,0 +1,14 @@
+#抓取的url
+
+##  http://www.2cto.com/
+	//http://www.2cto.com/meinv/sexmv/ 性感美女
+	//http://www.2cto.com/meinv/rhmeinv/ 比基尼
+	//http://www.2cto.com/meinv/gaoqing/ 美女诱惑
+	//http://www.2cto.com/meinv/meitui/  性感车模
+	// http://www.2cto.com/meinv/mote/  美女自拍
+	// http://www.2cto.com/meinv/xiaoqingxin/ 清纯美女
+	// http://www.2cto.com/meinv/mingxing/  明星图片
+	// http://www.2cto.com/meinv/top/   推荐图片
+	
+	
+###
