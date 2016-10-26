ae77ad5111d4a987c3884729eeb433630797c8d8
songqinghu <sqh2010304012@126.com>
2016-10-26 23:31:19 +0800

    update

--- GitDemo/src/main/resources/sql/bookname.sql
+++ GitDemo/src/main/resources/sql/bookname.sql
@@ -1,4 +1,36 @@
--- 加入网站的书籍列表 --
+-- 加入网站的书籍列表
 --不朽凡人 和 永夜君王
 INSERT INTO `bookwebspiderinfo` VALUES ('1', '1', 'http://m.biquge.com/21_21470/', '2');
 INSERT INTO `bookwebspiderinfo` VALUES ('2', '1', 'http://m.biquge.com/0_635/', '1');
+--大主宰
+INSERT INTO `bookwebspiderinfo` VALUES ('3', '1', 'http://m.biquge.com/0_176/', '1');
+--玄界之门
+INSERT INTO `bookwebspiderinfo` VALUES ('4', '1', 'http://m.biquge.com/13_13189/', '2');
+--逆鳞
+INSERT INTO `bookwebspiderinfo` VALUES ('5', '1', 'http://m.biquge.com/11_11896/', '3');
+--美人为馅
+INSERT INTO `bookwebspiderinfo` VALUES ('6', '1', 'http://m.biquge.com/3_3849/', '3');
+--元气少年
+INSERT INTO `bookwebspiderinfo` VALUES ('7', '1', 'http://m.biquge.com/35_35256/', '4');
+--终极教师
+INSERT INTO `bookwebspiderinfo` VALUES ('8', '1', 'http://m.biquge.com/2_2846/', '4');
+--斗破苍穹
+INSERT INTO `bookwebspiderinfo` VALUES ('9', '1', 'http://m.biquge.com/0_390/', '5');
+--赘婿
+INSERT INTO `bookwebspiderinfo` VALUES ('10', '1', 'http://m.biquge.com/0_285/', '5');
+--盗墓笔记
+INSERT INTO `bookwebspiderinfo` VALUES ('11', '1', 'http://m.biquge.com/0_846/', '6');
+--神武
+INSERT INTO `bookwebspiderinfo` VALUES ('12', '1', 'http://m.biquge.com/0_657/', '6');
+--盛唐崛起
+INSERT INTO `bookwebspiderinfo` VALUES ('13', '1', 'http://m.biquge.com/10_10982/', '7');
+--极品家丁
+INSERT INTO `bookwebspiderinfo` VALUES ('14', '1', 'http://m.biquge.com/0_404/', '7');
+--神级英雄
+INSERT INTO `bookwebspiderinfo` VALUES ('15', '1', 'http://m.biquge.com/0_136/', '8');
+--惊悚乐园
+INSERT INTO `bookwebspiderinfo` VALUES ('16', '1', 'http://m.biquge.com/0_156/', '8');
+--斗破苍穹之无上之境
+INSERT INTO `bookwebspiderinfo` VALUES ('17', '1', 'http://m.biquge.com/1_1431/', '9');
+--霸王硬上弓
+INSERT INTO `bookwebspiderinfo` VALUES ('18', '1', 'http://m.biquge.com/28_28255/', '9');
