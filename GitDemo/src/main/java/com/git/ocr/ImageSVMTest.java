6312eaf31960f505ad47aa17721fc53249da5484
songqinghu <sqh2010304012@126.com>
2016-12-06 22:40:54 +0800

    代码不全 暂时放下  -- 第五章有svm的内容 学会了 尝试补全一下

---
+++ GitDemo/src/main/java/com/git/ocr/ImageSVMTest.java
@@ -0,0 +1,27 @@
+package com.git.ocr;
+/**
+ * libsvm 识别图片
+ * <p>Title: ImageSVMTest.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2016</p>
+ * <p>Company: Sage</p>
+ * @author 五虎将
+ * @date 2016年12月6日下午10:27:17
+ * @version 1.0
+ */
+public class ImageSVMTest {
+
+	public static void main(String[] args) {
+		
+		int featureNum = Entry.DOWNSAMPLE_HEIGHT*Entry.DOWNSAMPLE_WIDTH +1;
+		
+		 
+		
+		
+		
+		
+		
+	}
+	
+	
+}
